36f751495c4e87617fcf2e5384d6b3db52bccca0
James Adarich <jamesrichford@googlemail.com>
2018-11-30 00:39:38 +0000

    seo updates (#24)
    * move robots.txt so exposed by gatsby
    
    * add sitemap to robots
    
    * optimise keywords, title and description
    
    * update links
    
    * lint

--- src/components/post-link.tsx
+++ src/components/post-link.tsx
@@ -7,12 +7,17 @@ interface PostLinkProps {
 }
 
 export default function PostLink({ post }: PostLinkProps) {
+  // ensure no redirect for non JS users / crawlers
+  const postUrl = post.frontmatter.path.endsWith("/")
+    ? post.frontmatter.path
+    : `${post.frontmatter.path}/`;
+
   return (
     <div className="post-link">
       <div>
         <h2>{post.frontmatter.title}</h2>
         <p>{post.frontmatter.description}</p>
-        <Link className="read-more-link" to={post.frontmatter.path}>
+        <Link className="read-more-link" to={postUrl}>
           read more
         </Link>
       </div>
