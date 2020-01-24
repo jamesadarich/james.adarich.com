106931119072f7cac116d4cac4585c74ed7ee109
James Adarich <jamesrichford@googlemail.com>
2020-01-24 23:15:38 +0000

    open graph (#27)
    * add footer
    
    * add open graph stuff
    
    * change graph query
    
    * rework to static query
    
    * fix linting issues

--- src/layouts/page.tsx
+++ src/layouts/page.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Helmet from "react-helmet";
 import SiteShell from "./index";
+import { graphql, StaticQuery } from "gatsby";
 
 interface PageProps {
   readonly title: string;
@@ -27,18 +28,50 @@ export abstract class Page extends React.PureComponent<PageProps> {
     return (
       <SiteShell>
         <div className="page-container">
-          <Helmet
-            title={`James Adarich - ${this.props.title}`}
-            meta={[
-              { name: "description", content: this.props.description },
-              { name: "keywords", content: this.keywords.join(", ") }
-            ]}
-          >
-            <html lang="en" />
-          </Helmet>
+          <StaticQuery
+            query={seoQuery}
+            render={({
+              site: {
+                siteMetadata: { siteUrl }
+              }
+            }) => (
+              <Helmet
+                title={`James Adarich - ${this.props.title}`}
+                meta={[
+                  { name: "description", content: this.props.description },
+                  { name: "keywords", content: this.keywords.join(", ") },
+                  {
+                    name: "image",
+                    content: `${siteUrl}/icons/icon-512x512.png`
+                  },
+                  { property: "og:title", content: this.props.title },
+                  {
+                    property: "og:description",
+                    content: this.props.description
+                  },
+                  {
+                    property: "og:image",
+                    content: `${siteUrl}/icons/icon-512x512.png`
+                  }
+                ]}
+              >
+                <html lang="en" />
+              </Helmet>
+            )}
+          />
           {this.props.children}
         </div>
       </SiteShell>
     );
   }
 }
+
+const seoQuery = graphql`
+  query SEO {
+    site {
+      siteMetadata {
+        siteUrl
+      }
+    }
+  }
+`;
