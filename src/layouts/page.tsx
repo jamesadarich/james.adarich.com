83640acfab0f297323fb17cc3a30f7c258015e68
James Adarich <jadarich@neworbit.co.uk>
2020-01-24 23:08:56 +0000

    rework to static query

--- src/layouts/page.tsx
+++ src/layouts/page.tsx
@@ -1,17 +1,12 @@
 import * as React from "react";
 import Helmet from "react-helmet";
 import SiteShell from "./index";
-import { graphql } from "gatsby";
+import { graphql, StaticQuery } from "gatsby";
 
 interface PageProps {
   readonly title: string;
   readonly description: string;
   readonly keywords?: Array<string>;
-  readonly site: {
-    siteMetadata: {
-      siteUrl: string;
-    }
-  }
 }
 
 export abstract class Page extends React.PureComponent<PageProps> {
@@ -33,19 +28,24 @@ export abstract class Page extends React.PureComponent<PageProps> {
     return (
       <SiteShell>
         <div className="page-container">
-          <Helmet
-            title={`James Adarich - ${this.props.title}`}
-            meta={[
-              { name: "description", content: this.props.description },
-              { name: "keywords", content: this.keywords.join(", ") },
-              { name: "image", content: `${this.props.site.siteMetadata.siteUrl}/icons/icon-512x512.png` },
-              { property: "og:title", content: this.props.title },
-              { property: "og:description", content: this.props.description },
-              { property: "og:image", content: `${this.props.site.siteMetadata.siteUrl}/icons/icon-512x512.png` }
-            ]}
-          >
-            <html lang="en" />
-          </Helmet>
+          <StaticQuery
+            query={seoQuery}
+            render={({ site: { siteMetadata: { siteUrl }}}) => (
+              <Helmet
+                title={`James Adarich - ${this.props.title}`}
+                meta={[
+                  { name: "description", content: this.props.description },
+                  { name: "keywords", content: this.keywords.join(", ") },
+                  { name: "image", content: `${siteUrl}/icons/icon-512x512.png` },
+                  { property: "og:title", content: this.props.title },
+                  { property: "og:description", content: this.props.description },
+                  { property: "og:image", content: `${siteUrl}/icons/icon-512x512.png` }
+                ]}
+              >
+                <html lang="en" />
+              </Helmet>
+            )}
+          />
           {this.props.children}
         </div>
       </SiteShell>
@@ -53,7 +53,7 @@ export abstract class Page extends React.PureComponent<PageProps> {
   }
 }
 
-export const pageQuery = graphql`{
+const seoQuery = graphql`query SEO {
   site {
     siteMetadata {
       siteUrl
